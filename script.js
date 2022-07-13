cc6640dadf48ce8c9194b2c7dc70e4eeb2653ed6
Raazkatwal <raazkatwal5@gmail.com>
2022-07-13 13:42:57 +0545

    added js

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelectorAll(".trending-btns");
+const TrendingTVshows = document.getElementsByClassName("trending-tv-shows")[0];
+const TrendingMovies = document.getElementsByClassName("trending-movies")[0];
+TrendingTVshows.style.display = 'none';
+btn[0].addEventListener("click", () => {
+    if (!btn[0].classList.contains('btn-active')) {
+        btn[0].classList.add('btn-active');
+        btn[1].classList.remove('btn-active');
+        TrendingTVshows.style.display = "none";
+        TrendingMovies.style.display = "grid";
+    }
+})
+btn[1].addEventListener("click", () => {
+    if (!btn[1].classList.contains('btn-active')) {
+        btn[1].classList.add('btn-active');
+        btn[0].classList.remove('btn-active');
+        TrendingMovies.style.display = "none";
+        TrendingTVshows.style.display = "grid";
+        }
+    })
