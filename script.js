2628c8f247b0a878b94117fba0b7c4e586f31bab
Raazkatwal <raazkatwal5@gmail.com>
2022-07-17 13:36:57 +0545

    Added login modal

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btn = document.querySelectorAll(".trending-btns");
 const TrendingTVshows = document.getElementsByClassName("trending-tv-shows")[0];
 const TrendingMovies = document.getElementsByClassName("trending-movies")[0];
+const loginbtn = document.querySelector(".login-btn");
+const Login_close_btn = document.querySelector(".form-close-btn");
+const popup = document.querySelector(".modal");
 TrendingTVshows.style.display = 'none';
 btn[0].addEventListener("click", () => {
     if (!btn[0].classList.contains('btn-active')) {
@@ -18,3 +21,9 @@ btn[1].addEventListener("click", () => {
         TrendingTVshows.style.display = "grid";
         }
     })
+loginbtn.addEventListener("click", () => {
+    popup.showModal();
+})
+Login_close_btn.addEventListener("click", () => {
+    popup.close();
+})
